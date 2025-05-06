ab64b31f18a966ff7bb71c5d37ba52dc05fb36ca
Eduardo Henrique <eduardo_hds3@outlook.com>
2025-05-05 22:07:21 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function togglemode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //Pegar a Tag IMG
+  // const img = document.querySelector("#profile img")
+
+  //Substituir a Imagem
+  //if (html.classList.contains("light")) {
+
+  //Se tiver light mode, adicionar a imagem light
+  //img.setAttribute("src", "./assets/avatar-light.png")
+  //}
+  //Se tiver sem light mode, manter a imagem normal.
+  //else {
+  //  img.setAttribute("src", "./assets/avatar.png")
+  // }
+}
